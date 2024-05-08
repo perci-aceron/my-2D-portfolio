19e51b3cc925cff81f8407e0bfe5ccaea924c7b4
Percival Aceron <perci.aceron@gmail.com>
2024-05-08 20:21:36 +0800

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my computer, where I attend my online course with GoIT, study and practice web development, and occasionally play online games.
+    Here is my <a href="https://github.com/perci-aceron" target="_blank">Github</a>!`,
+  "certificate": `This certificate from GoIT (coming soon) proudly adorns my wall. While it's primarily theoretical, I believe it provided a solid foundation that I'm proud of.`,
+  "sofa-table": `This sofa is my relaxation hub. when it comes to enjoying TV, a comfy sofa like this one is essential; standing just doesn't compare.`,
+  tv: `This is my TV! It's where I unwind, watch <a href="https://www.youtube.com/@GoIT_philippines" target="_blank">GoIT</a> programming tutorials on YouTube, stream movies, and dive into console gaming sessions.`,
+  bed: `This is my sleeping spot. I'm so good at sleeping, I can even do it while my eyes are closed.`,
+  resume: `This is my desk and on it is my resume. <a href="https://perci-aceron-ats-resume.vercel.app/" target="_blank">Check it out.</a>
+  Feel free to reach out to me at <a href="mailto:perci.aceron@gmail.com">perci.aceron@gmail.com</a>`,
+  projects: `This portfolio is created using the Kaboom.js library, designed specifically for building games in JavaScript. Explore Kaboom.js further by checking out tutorials to learn more about its capabilities.`,
+  library: `With an abundance of online resources available, who still purchases books, especially those unrelated to programming, when my shelves are already brimming with them?`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
+};
